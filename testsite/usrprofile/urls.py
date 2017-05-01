51d0d58722e5c4053eb0556de19c96a12afe9f28
rishirajjain <rishirajjain319@gmail.com>
2017-05-02 00:50:52 +0530

    Update

--- testsite/usrprofile/urls.py
+++ testsite/usrprofile/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.ListPost.as_view() ,name ='index'  ),
-    url(r'^(?P<pk>[0-9]+)$' ,views.ListPost.as_view() ,name ='Post'),
-    url(r'^(?P<pk>[0-9]+)$' ,views.Dview.as_view()),
+    url(r'^(?P<pk>[0-9]+)' ,views.ListPost.as_view() ,name ='Post'),
+    url(r'^detail/(?P<pk>[0-9]+)$' ,views.detailview.as_view(), name='details'),
+    url(r'^delete/(?P<pk>[0-9]+)$' ,views.DeletePost.as_view(), name='deletepost'),
 
 
 ]
--- testsite/usrprofile/views.py
+++ testsite/usrprofile/views.py
@@ -1,3 +1,5 @@
+
+from django.views.generic import DeleteView
 from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import TemplateView
@@ -12,5 +14,10 @@ class List(ListView):
     template_name = 'usrprofile/post.html'
     model = Post
 
-class Dview(DetailView):
+class detailview(DetailView):
     template_name = 'usrprofile/details.html'
+    model = Post
+
+class DeletePost(DeleteView):
+    model = Post
+    success_url = 'usrprofile/post_confirm_delete.html'
